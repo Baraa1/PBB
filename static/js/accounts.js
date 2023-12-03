e8c41421b6ba3186750b757910b3fa0b18ee8099
Baraa1 <baraa.amjad1@gmail.com>
2023-12-03 12:44:09 +0300

    bug fixes

--- static/js/accounts.js
+++ static/js/accounts.js
@@ -8,9 +8,12 @@ function deactivate_acc() {
 }
 
 function swap_text() {
-    const modalsbtns = document.querySelectorAll('.btn-danger')
+    const modalsbtns = document.querySelectorAll('.activation-btn')
+    console.log(modalsbtns)
     modalsbtns.forEach(btn => {
+        console.log('ac1')
         btn.addEventListener('click', () => {
+            console.log('ac2')
             const mbody = btn.parentElement.previousElementSibling;
             if(btn.textContent.includes('Activate')) {
                 btn.textContent = 'Deactivate';
